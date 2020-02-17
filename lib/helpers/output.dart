1cf1149632d8dc15f0b11de896a06af84b07d1d7
Ehsan Gharaei <ehsan.gharaei@ut.ac.ir>
2020-02-17 10:27:59 +0330

    DB Fixes

--- lib/helpers/output.dart
+++ lib/helpers/output.dart
@@ -12,7 +12,13 @@ class Output {
 
   void writeString(String fileName, String text) {
     if (text == null || fileName == null) return;
-    File(Directory.current.path + '/data/$fileName')
+    File file;
+    if (Platform.isMacOS)
+      file = File(Directory.current.parent.parent.parent.parent.path +
+          '/Desktop/$fileName');
+    else
+      file = File(Directory.current.path + '/data/$fileName');
+    file
       ..createSync(recursive: true)
       ..writeAsStringSync(text);
   }
